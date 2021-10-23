1f86e7f786ea28f2d445ddd038cb8fd540df46d1
Sunny <73135987+wzhao0842@users.noreply.github.com>
2021-10-22 20:12:48 -0700

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -18,3 +18,29 @@ int main(){
     }
     cout << cost[n-1];
 }
+
+//dice combination 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<int, int> memo;
+
+int main(){
+    int n; cin >> n;
+    int ar[n];
+    long long dp[n+5]; memset(dp, 0, sizeof(dp));
+    long long Mod = (long long)1e9 + 7;
+    for(int i=0;i<n;i++){
+        cin >> ar[i];
+    }
+    dp[0] = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=6;j++){
+            if(j <= i){
+                dp[i] += dp[i-j] % Mod;
+            }
+        }
+    }
+    cout << dp[n] % Mod;
+}
