995ebad9671502fde8c7f9dada12f3f8ba3ebcbf
Sunny <73135987+wzhao0842@users.noreply.github.com>
2021-10-22 23:26:36 -0700

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -44,3 +44,62 @@ int main(){
     }
     cout << dp[n] % Mod;
 }
+
+//minimizing coins
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+map<int, int> memo;
+ 
+int main(){
+    int INF = 1e9;
+    int n, x;
+    cin >> n >> x;
+    int coins[n];
+    for(int i=0;i<n;i++){
+        cin >> coins[i];
+    }
+    int dp[x+5]; 
+    for(int i=0;i<=x;i++) dp[i] = INF;
+    dp[0] = 0;
+    for(int i=0;i<=x;i++){
+        for(int v : coins){
+            if(v <= i){
+                dp[i] = min(dp[i], dp[i-v]+1);
+            }
+        }
+    }
+    if(dp[x] >= INF){
+        cout << -1 << endl;
+    }else{
+        cout << dp[x] << endl;
+    }
+}
+
+//dice combination 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+map<int, int> memo;
+ 
+int main(){
+    int n; cin >> n;
+    int ar[n];
+    long long dp[n+5]; memset(dp, 0, sizeof(dp));
+    long long Mod = (long long)1e9 + 7;
+    for(int i=0;i<n;i++){
+        cin >> ar[i];
+    }
+    dp[0] = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=6;j++){
+            if(j <= i){
+                dp[i] += dp[i-j] % Mod;
+            }
+        }
+    }
+    cout << dp[n] % Mod;
+}
